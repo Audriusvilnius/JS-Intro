43504ab820c273ebbe3030df1fe800e913c37f6c
audriusmacbook <audrius.ivko@gmail.com>
2022-10-18 13:13:53 +0300

    Sprendimai iki 1/2

---
+++ js/pratimai.js
@@ -0,0 +1,82 @@
+// Kintamųjų inicijavimas
+/* 1.	Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+a.	Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+const firsNumb = 1;
+const secondNumb = 2;
+const lastNumb = 3;
+
+console.log(firsNumb);
+console.log(secondNumb);
+console.log(lastNumb);
+
+console.log(firsNumb, secondNumb, lastNumb)
+
+/* 2.	Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+a.	Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+const firstString = "a";
+const secondString = "b";
+const lastString = "c";
+
+console.log(firstString);
+console.log(secondString);
+console.log(lastString);
+
+console.log(firstString, secondString, lastString);
+
+/*3.	Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+a.	Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+/*4.	Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+a.	Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+
+
+
+
+//Veiksmai su kintamaisiais
+
+/* 1.	Susumuoti visus skaičiaus tipo kintamuosius
+a.	Rezultatą išvesti į console
+*/
+
+const sumNumb = firsNumb+secondNumb+lastNumb;
+console.log(sumNumb);
+
+const sumNumb1 = `${firsNumb} + ${secondNumb} * ${lastNumb} = ${firsNumb + secondNumb * lastNumb }`;
+
+console.log(sumNumb1);
+
+/*2.	Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+a.	Rezultatą išvesti į console
+*/
+
+const sumString = firstString + ' ' + secondString + ' ' + lastString;
+console.log(sumString)
+console.log(lastString + ' ' + secondString + ' ' + firstString);
+
+/*3.	Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+a.	1-2+3-4+5
+b.	Rezultatą išvesti į console
+*/
+
+const p1=1;
+const p2=2;
+const p3=3;
+const p4=4;
+const p5=5;
+mathP=p1-p2+p3-p4+p5;
+
+console.log(mathP);
+console.log(p1-p2+p3-p4+p5);
+
+/*4.	Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas
+*/
+
+
+
