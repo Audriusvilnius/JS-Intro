f36db5f045ee84223c2541dc16c81468615cec93
audriusmacbook <audrius.ivko@gmail.com>
2022-10-18 10:50:10 +0300

    test

---
+++ js/02-string.js
@@ -0,0 +1,13 @@
+/* String - tekstas
+
+*/
+
+const firsName = 'Petras';
+console.log(firstName);
+
+const lastName = "Jonaitis";
+console.log(lastName);
+
+const fullName = firstName +""+ lastName;
+console.log(fullName);
+
