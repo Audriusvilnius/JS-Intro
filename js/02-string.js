021fbed2b70710a95fd7a56a114a89f6c7756781
audriusmacbook <audrius.ivko@gmail.com>
2022-10-18 10:57:20 +0300

    string

--- js/02-string.js
+++ js/02-string.js
@@ -11,3 +11,13 @@ console.log(lastName);
 const fullName = firstName +""+ lastName;
 console.log(fullName);
 
+console.log("2" + "2");
+console.log(2 + 2);
+console.log(2 + "2");
+console.log("2" + 2);
+
+
+
+
+
+
