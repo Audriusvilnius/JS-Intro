f910eddb54550ab36789fcde3106f52572a3750e
audriusmacbook <audrius.ivko@gmail.com>
2022-10-19 10:42:46 +0300

    stilius

--- js/04-array.js
+++ js/04-array.js
@@ -7,14 +7,14 @@ const paymys4 = 6;
 const paymys5 = 9;
 
 
-const vid = (paymys1+paymys2+paymys3+paymys4+paymys5)/5;
+const vid = (paymys1 + paymys2 + paymys3 + paymys4 + paymys5) / 5;
 console.log(`vid: ${vid}`);
 
-const mokiniai =['Petras', 'Jonas', 'Maryte'];
+const mokiniai = ['Petras', 'Jonas', 'Maryte'];
 
 const turiPapeikima = [true, false, false];
 
-const pazymiai = [10,2,8,4,6];
+const pazymiai = [10, 2, 8, 4, 6];
 console.log(pazymiai);
 
 const ap1 = pazymiai[0];
@@ -24,20 +24,20 @@ const ap4 = pazymiai[3];
 const ap5 = pazymiai[4];
 
 const kiekis = pazymiai.length;
-console.log("Masyvo i = ",kiekis);
+console.log("Masyvo i = ", kiekis);
 
 console.log(ap1);
 console.log(ap4);
 
-console.log(ap1,ap2,ap3,ap4,ap5);
+console.log(ap1, ap2, ap3, ap4, ap5);
 
 
 const pirkiniai = [
     ['agurkas', 2],
-    ['pomidoras',4],
+    ['pomidoras', 4],
     ['pipirai', 6],
-    ['cesnakas',7],
-    ['burokus',5]
+    ['cesnakas', 7],
+    ['burokus', 5]
 ];
 
 console.log(pirkiniai);
@@ -47,7 +47,7 @@ console.log(pirkiniai);
 
 const prekesIndex = 0;
 
-const preke =  pirkiniai[prekesIndex];
+const preke = pirkiniai[prekesIndex];
 const prekesKiekis = preke[1];
 const prekesPavadinimas = preke[0];
 
@@ -55,7 +55,7 @@ console.log(preke);
 console.log(prekesKiekis);
 console.log(prekesPavadinimas);
 
-console.log( `reiknu pirkti ${prekesPavadinimas} ${prekesKiekis}`);
+console.log(`reiknu pirkti ${prekesPavadinimas} ${prekesKiekis}`);
 
 // mokykla > klases > mokiniai > tevus > telefonai
 const mokykla = [
