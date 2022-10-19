8132ebc0f17d879a24d4de752568e0aae2ff776c
audriusmacbook <audrius.ivko@gmail.com>
2022-10-19 13:31:58 +0300

    pazaidimas su matricom ir objektais

--- js/05-object.js
+++ js/05-object.js
@@ -35,46 +35,64 @@ console.log(marytesIsMarred);
 
 
 const mokykla = {
-    1:[
-        {
-            name: 'Petras',
-            tevai: {
-                tete:[112],
-                mama:[911],
+    1: [
+        { name: 'Petras', 
+            tevai: { 
+                tete: [112],
+                mama: [111]
             }
         },
-        {
-            name: 'Maryte',
-            tevai: {
-                tete:[112112],
-                mama:[911911],
+        { name: 'Maryte',
+            tevai: { 
+                tete: [112112],
+                mama: [111911]
             }
         },
     ],
-    2:[
-        {
-            name: 'Jonas',
+    2: [
+        { name: 'Jonas',
             tevai: {
-                tete:[1121],
-                mama:[333444],
+                tete: [211],
+                mama: [219]
             }
         },
-        {
-            name: 'Ona',
+        { name: 'Ona',
             tevai: {
-                tete:[333],
-                mama:[911911,112],
+                tete: [211211],
+                mama: [219119, 299]
+            }
+        },
+    ],
+    3: [
+        { name: 'Deivis',
+            tevai: {
+                tete: [311],
+                mama: [344]
+            }
+        },
+        { name: 'Birute',
+            tevai: {
+                tete: [311311],
+                mama: [344344, 312]
             }
         },
     ],
 
 }
 
-const klase = 2;
-const mokinioIndexas = 1;
-const kurisTevu = 'tete';
-const klasesInfo = mokykla[klase];
-console.log(klase);
-const kazkas = mokykla[klase][mokinioIndexas].tevai[kurisTevu][0];
-//const kazkas = mokykla[1][1].tevai.mama[0];
-console.log(kazkas);
+//const klase = 2;
+//const mokinioIndexas = 0;
+//const kurisTevu = 'tete';
+//const klasesInfo = mokykla[klase];
+//console.log(klase);
+
+//const kazkas1= mokykla[klase][mokinioIndexas].tevai[kurisTevu][0];
+//const kazkas2 = mokykla[1][1].tevai.mama[0];
+//const kazkas3 = mokykla.klase;
+//const test1 = mokykla[3][1].tevai.mama[0];
+const test2 = mokykla[3][0].tevai.mama[0];
+//console.log(kazkas1);
+//console.log(kazkas2);
+//console.log(kazkas3);
+//console.log(test1);
+console.log(test2);
